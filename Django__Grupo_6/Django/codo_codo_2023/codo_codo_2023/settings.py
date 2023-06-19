8c3743a876a1c7b00ff3610350257542f4aa8b65
VaninaCortez <vany_vc@hotmail.com>
2023-06-19 01:57:46 -0300

    dando estilo y color a las vistas

--- Django__Grupo_6/Django/codo_codo_2023/codo_codo_2023/settings.py
+++ Django__Grupo_6/Django/codo_codo_2023/codo_codo_2023/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4!^a@b8#o&-pkir6t#%8ncfj3^!!mbp1#e*o^0$(wa5w!3lf@n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -123,9 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Viajeros/static'), 
                     os.path.join(BASE_DIR, 'media')
                     )
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = 'media/'
